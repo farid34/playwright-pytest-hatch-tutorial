6317ead477d75ac150cd6267a6dec8ef534fc43f
Farid Abdelhamid <farid.abdelhamid1@gmail.com>
2025-12-09 21:44:43 +0100

    Add authent locked user test

--- tests/e2e/test_login.py
+++ tests/e2e/test_login.py
@@ -15,3 +15,14 @@ def test_login_success(login_page):
 
     # Vérifier qu'on est redirigé vers la page inventory
     expect(login_page.app_logo).to_have_text("Swag Labs")
+
+
+@pytest.mark.smoke
+def test_login_locked_user(login_page):
+    """Test: Login avec un utilisateur bloqué"""
+    login_page.navigate()
+    login_page.login("locked_out_user", "secret_sauce")
+
+    assert login_page.is_error_displayed
+    error_message = login_page.get_error_message()
+    assert "Sorry, this user has been locked out" in error_message
